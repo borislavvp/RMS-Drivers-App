f4e82a283283685dedeff15f0002d7f64b3c659a
Borislav Pavlov <416657@student.fontys.nl>
2021-06-02 00:53:55 +0300

    implemented the main pages and auth view changes

---
+++ src/pages/Main.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {  IonText, IonPage, IonContent,} from '@ionic/react';
+
+export const Main: React.FC = () => {
+    
+	return (
+        <IonPage>
+            <IonContent>
+                <IonText>
+                    Chorba Driver
+                </IonText>
+            </IonContent>
+        </IonPage>
+	);
+};

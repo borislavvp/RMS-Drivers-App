a1544db0c2033d9dbacc3a47ba881f7a8b2a62f8
Borislav Pavlov <416657@student.fontys.nl>
2021-06-02 00:53:40 +0300

    added login pages

---
+++ src/pages/login/SignOutRedirect.tsx
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react';
+import { useHistory } from 'react-router';
+import { useStoreActions } from '../../store';
+
+export const SignOutRedirect: React.FC = () => {
+    const handleLogoutCallback = useStoreActions((state) => state.authenticationService.handleLogoutCallback)
+    const history = useHistory();
+    
+    useEffect(() => {
+        (async () => {
+            try {
+                await handleLogoutCallback();
+                history.push("/")
+            } catch (error) {
+                console.log(error)
+                history.push("/")
+            }
+        })()
+    }, []);
+    
+    return (null);
+};

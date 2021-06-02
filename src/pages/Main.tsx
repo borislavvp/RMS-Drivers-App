7a8705e447e49b47abb4b3d19a539ef43c281003
Borislav Pavlov <416657@student.fontys.nl>
2021-06-02 21:34:10 +0300

    added base implementation of the order available screen

--- src/pages/Main.tsx
+++ src/pages/Main.tsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import {  IonText, IonPage, IonContent,} from '@ionic/react';
+import React, { useEffect } from 'react';
+import {  IonText, IonPage, IonContent, IonAlert,} from '@ionic/react';
+import { socket, useStoreActions, useStoreState } from '../store';
 
 export const Main: React.FC = () => {
+    const removeOrderFromStack = useStoreActions(actions => actions.ordersService.removeOrderFromStack);
+    const pushOrderToStack = useStoreActions(actions => actions.ordersService.pushOrderToStack);
     
+    const orderAvailable = useStoreState(state => state.ordersService.orderAvailable);
+
+    useEffect(() => {
+        socket.on.OrderReadyForPickup = pushOrderToStack;
+        socket.on.OrderStatusChange = removeOrderFromStack;
+    }, []);
+
 	return (
         <IonPage>
             <IonContent>
                 <IonText>
                     Chorba Driver
                 </IonText>
+                {orderAvailable.orderNumber !== undefined && <IonAlert
+                    isOpen={true}
+                    header="Order available!"
+                    cssClass="rounded-lg shadow-lg"
+                    message={`${orderAvailable.orderNumber}`}
+                    buttons={['OK']}
+                />}
             </IonContent>
         </IonPage>
 	);
